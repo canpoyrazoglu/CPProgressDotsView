5fc2bfded7da14bf17a705d62ac1eb5e89935e36
Can Poyrazoğlu <can@canpoyrazoglu.com>
2018-05-15 10:40:59 +0300

    initial commit

---
+++ CPProgressDotsView/Classes/CPProgressDotsView.h
@@ -0,0 +1,13 @@
+//
+//  CPProgressDotsView.h
+//  CPProgressDotsView
+//
+//  Created by Can Poyrazoğlu on 15.05.2018.
+//  Copyright © 2018 Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPProgressDotsView : UIView
+
+@end
---
+++ CPProgressDotsView/Classes/CPProgressDotsView.m
@@ -0,0 +1,21 @@
+//
+//  CPProgressDotsView.m
+//  CPProgressDotsView
+//
+//  Created by Can Poyrazoğlu on 15.05.2018.
+//  Copyright © 2018 Can. All rights reserved.
+//
+
+#import "CPProgressDotsView.h"
+
+@implementation CPProgressDotsView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
